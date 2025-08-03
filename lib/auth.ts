176f755d69157adf4cdcd88edd20f38dfdbefbc9
Sahil Jadhav <sahiljadhav1617@gmail.com>
2025-08-03 16:23:17 +0530

    All Files Updated

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,4 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDatabase } from "./db";
 import User from "@/models/User";
@@ -40,14 +38,6 @@ export const authOptions: NextAuthOptions = {
                 }
             }
         }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
     ],
     callbacks:{
         async jwt({ token, user}) {
